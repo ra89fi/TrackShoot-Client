0ad473884c1f866b89fda25a74e91e39c9f29e54
raihan <5421449+ra89fi@users.noreply.github.com>
2022-05-02 22:43:19 +0600

    item route

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,28 +5,53 @@ const Item = () => {
     const params = useParams();
     const [item, setItem] = useState({});
     useEffect(() => {
-        fetch('products.json')
+        fetch(`${process.env.REACT_APP_BACK_URL}/items/${params.id}`)
             .then((data) => data.json())
-            .then((products) =>
-                setItem(products.filter((item) => item.id === params.id)[0])
-            )
+            .then((item) => setItem(item))
             .catch((err) => console.log(err.message));
     }, []);
+    const keys = ['_id', 'image'];
+    const onDelivered = () => {};
+    const onAdd = () => {};
     return (
-        <div>
-            {Object.keys(item).map((key) => (
-                <p>{item[key]}</p>
-            ))}
-            <p>
-                <button className="btn btn-danger">Delivered</button>
-            </p>
-            <p>
-                <input type="number" /> &nbsp;{' '}
-                <button className="btn btn-warning">Add to Stock</button>
-            </p>
-            <p>
-                <a href="/inventory">Manage Inventories</a>
-            </p>
+        <div style={{ width: '65%', margin: '60px auto' }}>
+            <div style={{ display: 'flex' }}>
+                <img src={item.image} alt={item.name} />
+                <div style={{ marginLeft: '20px' }}>
+                    {Object.keys(item).map(
+                        (key) =>
+                            !keys.includes(key) && (
+                                <p key={key}>
+                                    {key[0].toUpperCase() + key.slice(1)} :{' '}
+                                    {item[key]}
+                                </p>
+                            )
+                    )}
+                </div>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    marginTop: '20px',
+                }}
+            >
+                <p>
+                    <button className="btn btn-danger" onClick={onDelivered}>
+                        Delivered
+                    </button>
+                </p>
+                <p style={{ display: 'flex', alignItems: 'center' }}>
+                    <input type="number" /> &nbsp;{' '}
+                    <button className="btn btn-warning" onClick={onAdd}>
+                        Add to Stock
+                    </button>
+                </p>
+                <p>
+                    <a href="/inventory">Manage Inventories</a>
+                </p>
+            </div>
         </div>
     );
 };
